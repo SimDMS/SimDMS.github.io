af8ed30412b3d4f35e6d70cda64d044c577a57db
SimDMS <maksim.dzhuzhuk@gmail.com>
2019-01-31 00:29:33 +0200

    startup

---
+++ startup/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.about__slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
+				
