564cac8f83ffdaec6fcf7f31cf12d65c4c11c04e
SimDMS <maksim.dzhuzhuk@gmail.com>
2019-02-01 19:11:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,6 +95,9 @@ $(".owl-carousel").owlCarousel({
   loop:true,
 });
 
+// -----плавный переход по якорям
+$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().top},1000),!1}})});
+// ------------------------------------------------
 
 
 
